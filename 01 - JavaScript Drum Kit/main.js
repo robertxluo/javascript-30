9026597670cfd49b4d30d8570565e6cf09abdcfb
Robert <robertxluo@gmail.com>
2018-09-24 12:28:47 -0700

    Fix formatting

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -8,7 +8,6 @@ const playSound = (e) => {
     key.classList.add('playing');
 }
 
-
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
