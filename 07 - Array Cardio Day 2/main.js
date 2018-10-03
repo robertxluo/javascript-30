8174d3f910687c159c36455e1469174b411a2d8e
Robert <robertxluo@gmail.com>
2018-10-03 12:23:32 -0700

    Complete array find function

--- 07 - Array Cardio Day 2/main.js
+++ 07 - Array Cardio Day 2/main.js
@@ -21,6 +21,7 @@ const currentYear = new Date().getFullYear();
     // Array.prototype.some() // is at least one person 19 or older?
     const isAdult = people.some((person) => currentYear - person.year >= 19);
     console.log(isAdult);
+
     // Array.prototype.every() // is everyone 19 or older?
     const isEveryoneAdult = people.every((person) => currentYear - person.year >= 19);
     console.log(isEveryoneAdult);
@@ -28,6 +29,8 @@ const currentYear = new Date().getFullYear();
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
+    const idFind = comments.find((comment) => comment.id === 823423);
+    console.log(idFind);
 
     // Array.prototype.findIndex()
     // Find the comment with this ID
