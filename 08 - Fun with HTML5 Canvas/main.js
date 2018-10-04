346c5b606ab5b2c8e5cb4395f4e8bd15248b379f
Robert <robertxluo@gmail.com>
2018-10-04 09:28:44 -0700

    Add drawing on canvas functionality

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -2,9 +2,10 @@ const canvas = document.querySelector('#draw');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-context.strokeStyle = '#BADA55';
+context.strokeStyle = 'red';
 context.lineJoin = 'round';
 context.lineCap = 'round';
+context.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
@@ -13,9 +14,18 @@ let lastY = 0;
 const draw = (e) => {
   if(!isDrawing) return;
   console.log(e);
+  context.beginPath();
+  context.moveTo(lastX, lastY);
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 };
 
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
