eb322fbdc167754d88a2775a6dc0a618b23e4534
Robert <robertxluo@gmail.com>
2018-10-04 09:23:38 -0700

    Implement mouse drag and drop functionality

---
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector('#draw');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.strokeStyle = '#BADA55';
+context.lineJoin = 'round';
+context.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+const draw = (e) => {
+  if(!isDrawing) return;
+  console.log(e);
+};
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
