34e1ac32ad617848e262b73d98c090afa23703e7
Robert <robertxluo@gmail.com>
2018-10-06 08:37:44 -0700

    Add dev tool tricks

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -1,4 +1,4 @@
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'Hugo', age: 8 }];
 
 function makeGreen() {
   const p = document.querySelector('p');
@@ -7,25 +7,58 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('Hello I am a %s string!', 'nice');
 
 // Styled
+console.log('%c I am some great text', 'font-size: 50px; background: red; text-shadow: 10px 10px 0 blue');
 
 // warning!
+console.warn('Oh noo!')
 
 // Error :|
+console.error('Shit!');
 
 // Info
+console.log('Crocodiles eat 3-4 people per year');
 
 // Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong');
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p);
+
+console.clear();
 
 // Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`)
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+})
 
 // counting
+console.count('Wes');
+console.count('Wes');
+console.count('Steve')
+console.count('Wes');
 
 // timing
+console.time('Fetching data');
+fetch('https://api.github.com/users/robertxluo')
+    .then(data => data.json())
+    .then(data => {
+      console.timeEnd('Fetching data');
+      console.log(data);
+    });
+
+console.table(dogs);
