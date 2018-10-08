d53ddbb8d6931038f43e7bae0167a6778b5aaaf0
Robert <robertxluo@gmail.com>
2018-10-08 10:14:36 -0700

    Implement skip function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
@@ -18,12 +18,14 @@ const updateButton = () => {
   toggle.textContent = icon;
 }
 
+const skip = () => {
+  video.currentTime += parseFloat(event.target.dataset.skip);
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
-
-
 toggle.addEventListener('click', togglePlay);
-
+skipButtons.forEach(button => button.addEventListener('click', skip))
