ae8ba47d64cc718e4130d99b40e2049643541614
Robert <robertxluo@gmail.com>
2018-10-12 20:12:23 -0700

    Refactor code

--- 15 - LocalStorage/main.js
+++ 15 - LocalStorage/main.js
@@ -2,36 +2,48 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-const addItem = (e) => {
-  e.preventDefault();
-  const text = (event.target.querySelector('[name=item')).value;
+function addItem(event) {
+  event.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
   const item = {
     text,
     done: false
-  }
+  };
 
-  items.push(item)
-  populateList(items, itemsList)
+  items.push(item);
+  populateList(items, itemsList);
   localStorage.setItem('items', JSON.stringify(items));
-  event.target.reset();
+  this.reset();
 }
 
-const populateList = (plates = [], platesList) => {
-  platesList.innerHTML = plates.map((plate, i) => {
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((item, index) => {
     return `
-      <li>
-        <input 
-          type="checkbox" 
-          data-index=${i} 
-          id="item${i}" 
-          ${plate.done ? 'checked' : ''}
-          />
-        <label for="">${plate.text}</label>
-      </li>
-    `;
+            <li>
+              <input 
+                type="checkbox" 
+                data-index=${ index} 
+                id="item${index}" 
+                ${ item.done ? 'checked' : ''} 
+              />
+              <label for="item${ index}">${item.text}</label>
+            </li>
+           `
   }).join('');
 }
 
-addItems.addEventListener('submit', addItem)
+function toggleDone(event) {
+  if (!event.target.matches('input')) return;
+  const element = event.target;
+  const indexOfChecked = element.dataset.index;
+
+  items[indexOfChecked].done = !items[indexOfChecked].done;
+
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
