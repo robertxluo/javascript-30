1ed136decf79d8ff90e0b51e134d30063d16dfa4
Robert <robertxluo@gmail.com>
2018-10-12 18:19:53 -0700

    Add function to populate list

---
+++ 15 - LocalStorage/main.js
@@ -0,0 +1,34 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+const addItem = (e) => {
+  e.preventDefault();
+  const text = (event.target.querySelector('[name=item')).value;
+  const item = {
+    text,
+    done: false
+  }
+
+  items.push(item)
+  populateList(items, itemsList)
+  event.target.reset();
+}
+
+const populateList = (plates = [], platesList) => {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input 
+          type="checkbox" 
+          data-index=${i} 
+          id="item${i}" 
+          ${plate.done ? 'checked' : ''}
+        />
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+addItems.addEventListener('submit', addItem)
