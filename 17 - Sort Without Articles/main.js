478bec0a1a388569c7edf2c05ba49930204b8c1b
Robert <robertxluo@gmail.com>
2018-10-17 16:30:00 -0700

    Change strip function to arrow function

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,10 +1,8 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+const strip = (bandName) => bandName.replace(/^(a |the |an )/i, '').trim();
 
-function strip(bandName) {
-  return bandName.replace(/^(a |the |an )/i, '').trim();
-}
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
 document.querySelector('#bands').innerHTML = 
   sortedBands
