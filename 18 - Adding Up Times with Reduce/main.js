e3d41ccd59c83267f06e926479ef65a1a853ba28
Robert <robertxluo@gmail.com>
2018-10-21 14:56:46 -0700

    Add array to get all the timestamps in seconds

---
+++ 18 - Adding Up Times with Reduce/main.js
@@ -0,0 +1,9 @@
+const timeNodes = [...document.querySelectorAll('[data-time]')];
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins * 60)  + secs;
+  });
+  
