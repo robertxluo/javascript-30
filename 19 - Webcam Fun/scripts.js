5cfb016bf48d287539420c7ceaf0ca8850b35f0a
Robert <robertxluo@gmail.com>
2018-10-30 11:44:13 -0700

    Add getVideo function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(error => {
+      console.error(`OH NO!!!`, err);
+    })
+}
+
+getVideo();
